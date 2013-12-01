3f49d00bd74120918efb6c0d0957e10e245edf92
Jonathan <jonathan@jonathan-pc.(none)>
2013-12-01 20:56:54 -0300

    Actualiza el estilo, mantenedor y completa el login

--- login.php
+++ login.php
@@ -1,121 +1,71 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>CAFETERIA</title>
-        <meta charset="UTF-8" />
-        <link rel="shortcut icon" href="coffee.ico" />
-        <link rel="shortcut icon" href="image/now.ico" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-        <meta name="description" content="Elastic Image Slideshow with Thumbnail Preview" />
-        <meta name="keywords" content="jquery, css3, responsive, image, slider, slideshow, thumbnails, preview, elastic" />
-        <meta name="author" content="Codrops" />
-
-        <link rel="shortcut icon" href="../favicon.ico"> 
-        <link rel="stylesheet" type="text/css" href="css/demo.css" />
-        <link rel="stylesheet" type="text/css" href="css/style.css" />
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Playfair+Display:400italic' rel='stylesheet' type='text/css' />
-		<noscript>
-		<link rel="stylesheet" type="text/css" href="css/noscript.css" />
-		</noscript>
+    <meta charset="UTF-8" />
+    <link rel="shortcut icon" href="coffee.ico" />
+    <link rel="shortcut icon" href="image/now.ico" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <meta name="description" content="Elastic Image Slideshow with Thumbnail Preview" />
+    <meta name="keywords" content="jquery, css3, responsive, image, slider, slideshow, thumbnails, preview, elastic" />
+    <meta name="author" content="Codrops" />
+    <link rel="shortcut icon" href="../favicon.ico"> 
+    <link rel="stylesheet" type="text/css" href="css/demo.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Playfair+Display:400italic' rel='stylesheet' type='text/css' />
+    <noscript>
+        <link rel="stylesheet" type="text/css" href="css/noscript.css" />
+    </noscript>
 </head>
-
 <body>
-     	       <div class="container">
-            <div class="header">
-              
-                <span class="right">
-                    <a href="">Ingresar</a>
-                    <a href="registrar.php"><strong>Registrar</strong>
-                    </a>
-                </span>
-                <div class="clr"></div>
-            </div>
-           
-      
+    <div class="container">
+        <div class="header">      
+            <span class="right">
+                <a href="">Ingresar</a>
+                <a href="registrar.php"><strong>Registrar</strong></a>
+            </span>
+            <div class="clr"></div>
         </div>
+    </div>
+    <div id="menu">
+    <ul id="men">
+    <li><a href="index.php">Inicio</a></li>
+    <li><a href="productos.php">Productos</a></li>
+    <li><a href="eventos.php">Eventos</a></li>
+    <li><a href="contacto.php">Contacto</a></li>
 
-      
+    </ul>
 
-        <div id="menu">
-            <ul id="men">
-                <li><a href="index.php">Inicio</a></li>
-                <li><a href="productos.php">Productos</a></li>
-                <li><a href="eventos.php">Eventos</a></li>
-                <li><a href="contacto.php">Contacto</a></li>
-             
-            </ul>
+    </div>
 
-        </div>
+    <div>
+    <form>
 
-        <div>
-            <form>
-                
-            </form>
-        </div>
-        <br>
+    </form>
+    </div>
+    <br>
 
-        <?php 
+    <?php
+        include "mantenedor.php";
 
+        if (isset($_POST["login"])) {
+            if (login($_POST["user"],$_POST["password"])) {
+                # $_SESSION['userid']
+                setcookie('userid',$_POST["user"]);
+                echo '<div class="success">Su usuario es correcto.</div>';
+            } else {
+                echo '<div class="error">Su usuario es incorrecto, intente nuevamente.</div>';
+            }
+        }
+    ?>  
 
-            session_start(); 
-            include_once "conexion.php"; 
-              
-            function verificar_login($user,$password,&$result) { 
-                $sql = "SELECT * FROM usuarios WHERE usuario = '$user' and password = '$password'"; 
-                $rec = mysql_query($sql); 
-                $count = 0; 
-              
-                while($row = mysql_fetch_object($rec)) 
-                { 
-                    $count++; 
-                    $result = $row; 
-                } 
-              
-                if($count == 1) 
-                { 
-                    return 1; 
-                } 
-              
-                else 
-                { 
-                    return 0; 
-                } 
-            } 
-              
-            if(!isset($_SESSION['userid'])) 
-            { 
-                if(isset($_POST['login'])) 
-                { 
-                    if(verificar_login($_POST['user'],$_POST['password'],$result) == 1) 
-                    { 
-                        $_SESSION['userid'] = $result->idusuario; 
-                        header("location:index.php"); 
-                    } 
-                    else 
-                    { 
-                        echo '<div class="error">Su usuario es incorrecto, intente nuevamente.</div>'; 
-                    } 
-                } 
-            ?> 
-         
-            <form action="" method="post" class="login"> 
-          
-            <div><label>User</label><input name="user" type="text" ></div> 
-            <div><label>Pass</label><input name="password" type="password"></div> 
-            <div><input name="login" type="submit" value="login" ></div> 
-            
-            </form>
-           
-            <?php 
-            } else { 
-                echo 'Su usuario ingreso correctamente.'; 
-                echo '<a href="logout.php">Logout</a>'; 
-            } 
-            ?>
+        <form action="" method="post" class="login"> 
 
+        <div><label>User</label><input name="user" type="text" ></div> 
+        <div><label>Pass</label><input name="password" type="password"></div> 
+        <div><input name="login" type="submit" value="login" ></div> 
 
-     
+        </form>
 </body>
 </html>
--- mantenedor.php
+++ mantenedor.php
@@ -37,3 +37,43 @@ function creausuario($userin, $passin)
 
 	return $ret;
 }
+
+/**
+ * Busca el usuario en la base de datos, lo verifica y debuelve un true si existe junto con la contraseña 
+ *
+ * @return bool
+ **/
+function login($userin,$passin)
+{
+
+	global $host,$user,$pass,$db;
+	$ret = true;
+
+	$conect = mysql_connect($host,$user,$pass);
+	mysql_select_db($db,$conect);
+
+	if (mysqli_connect_errno())
+	{
+	  	$ret = false;
+	 	# die ("Failed to connect to MySQL: " . mysqli_connect_error() . "<br>");
+  	}
+
+  	if ($result = mysql_query("select * from usuarios where usuario = '$userin' and password = '$passin';",$conect))
+  	{
+  		$contador = 0;
+  		while ($fila = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    		$contador = $contador + 1;
+		}
+
+		if ($contador == 0){
+			$ret = false;
+		}
+  	} else {
+  		$ret = false;
+	    # die('Error: ' . mysqli_error($con)."<br>");
+  	}
+
+	mysql_close($conect);
+
+	return $ret;
+}
